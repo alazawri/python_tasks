4ec00aaafa545c461b835383a0cbba03fbbebd7a
alazwari <you@example.cogit config --global user.email aazwri@gmail.comgit config --global user.email you@example.co>
2020-03-29 12:59:36 +0300

    suppprt python 2 for task # 3

--- task0003/task0003.py
+++ task0003/task0003.py
@@ -1,5 +1,5 @@
+# python 3
 import random
-# file_name = ('./out.dat', 'w')
 the_generator = [random.sample(range(1, 51), 1) for i in range(1000000)]
 the_list = []
 for i in the_generator:
@@ -7,3 +7,14 @@ for i in the_generator:
 with open('./out.dat', 'w') as file_name:
     for i in range(1, 51):
         file_name.write((f'Number{i}, {the_list.count(i)}\n'))
+
+
+# python 2
+import random
+the_generator = [random.sample(range(1, 51), 1) for i in range(1000000)]
+the_list = []
+for i in the_generator:
+    the_list.extend(i)
+with open('./out.dat', 'w') as file_name:
+    for i in range(1, 51):
+        file_name.write(('Number' + str(i) +',' + str(the_list.count(i)) + '\n'))
